896c5f36d060409d2d9bfd45050069cb2a56bd62
Julio Cesar Jimenez Mora <cesar_driver.energy@hotmail.com>
2023-07-02 20:45:42 -0600

    Creacion de la funcion de registro
    Creacion de registro de alumno y arreglo con function de recibir la informacion

---
+++ reg_alumn.js
@@ -0,0 +1,43 @@
+function Alumno(id, names,apepaterno,apematerno, edad,curso,calificacion,grupo) {
+    this.id = id;
+    this.names =names;
+    this.apepaterno=apepaterno;
+    this.apematerno=apematerno;
+    this.edad = edad;
+    this.curso=curso;
+    this.calificacion=calificacion;
+    this.grupo=grupo;
+  }
+  var alumnos = [];
+  function obtenerMatricula(){
+  var obtenerM = document.getElementById("matricula").value;
+  
+  return obtenerM; 
+  }
+  function obtenerNombre() { 
+  var obtenerN = document.getElementById("nombre").value;
+
+  return obtenerN; 
+  }
+  function obtenerApepat(){
+  var obtenerAp=document.getElementById("apepaterno").value;
+  return obtenerAp;
+  }
+  function obtenerApemat(){
+  var obtenerAm=document.getElementById("apematerno").value;  
+  return obtenerAm;
+  }
+  function obtenerEdad() { // Obtengo la edad del input
+    var obtenerE = document.getElementById("edad").value;
+    obtenerE = parseInt(obtenerE); 
+    return obtenerE;
+  }
+  function crearAlumno() {
+    var registroAlu= new Alumno (obtenerMatricula(), obtenerNombre(),obtenerApepat(),obtenerApemat(), obtenerEdad(),'-','-','-');
+    alumnos.push(registroAlu);
+    mostrarListado();
+    ListaNuevos();
+    calcupro();
+   
+  }  
+ 
